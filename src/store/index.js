a0478be8c72f8165c957c0f9e1ad5eca386d68f8
q20096th <Erli.zhuang@toshiba-tih.com>
2020-10-30 20:32:00 +0800

    添加一些文件，

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 // store文件夹下index.js
+import fetch from "../assets/js/fetch";
 import Vue from "vue";
 import Vuex from "vuex";
 Vue.use(Vuex);
@@ -6,42 +7,69 @@ Vue.use(Vuex);
 import homeStore from "./homeStore";
 import loginStore from "./loginStore";
 import dashboard from "./dashboard";
+import ftpInfo from "./ftpInfo";
 import selectLine from "./selectLine";
 import audit from "./audit";
+import logs from "./logs";
+import mqtt from "./mqtt";
 import common from "./common";
 
-Vue.use(Vuex);
+
+
+//以下是些样例， 可以根据需要修改
 export default new Vuex.Store({
-  state: {
-    lastLinedata: {},
-    lastLineName: "",
-    lastStationdetaildata: {},
-    lastModeldetaildata: {},
-    CurrentSelectedModelID: ""
-  },
   modules: {
-    homeStore,
-    loginStore,
-    dashboard,
-    selectLine,
-    audit,
-    common
+    loginStore, // 登录状态
+    markTasks, // 标记任务
+    pictureTask, // 图片标签列表
+    modelManagement, // 训练模型结果
+    serverInfo, // 服务器信息
+    ftpInfo, // Ftp服务器信息
+    userManagement, // 用户管理
+    logs, // 训练日志
+    training_tasks, // 训练任务
+    training_models, // 训练模型详情
+    training_details, //训练详情
+    mqtt, // mqtt
+    common, // 通用
+    modelManagementAudit, // 模型审核
+    markTasksAudit, // 标记任务审核
+    templateTask // 标签模板的弹窗
+  },
+  state: {
+    modelData: [],
+    procedureData: []
   },
   mutations: {
-    changelastLineName(state, lastLineName) {
-      state.lastLineName = lastLineName;
+    setModelData(state, data) {
+      data.push({
+        id: "",
+        name: "全部"
+      });
+      state.modelData = data;
     },
-    changelastLinedata(state, lastLinedata) {
-      state.lastLinedata = lastLinedata;
+    setProcedureData(state, data) {
+      data.push({
+        id: "",
+        name: "全部"
+      });
+      state.procedureData = data;
+    }
+  },
+  actions: {
+    //获取机型列表
+    async getModelList({ commit }) {
+      // await fetch.get("/api/models/sync_mira_data")
+      let data = await fetch.get("/api/models");
+      commit("setModelData", data.data.models);
     },
-    changelastStationdetaildata(state, lastStationdetaildata) {
-      state.lastStationdetaildata = lastStationdetaildata;
-    },
-    changelastModeldetaildata(state, lastModeldetaildata) {
-      state.lastModeldetaildata = lastModeldetaildata;
-    },
-    changeCurrentSelectedModelID(state, CurrentSelectedModelID) {
-      state.CurrentSelectedModelID = CurrentSelectedModelID;
+    //获取指定机型下的工序
+    async getProcedureList({ commit }, stark) {
+      // await fetch.get("/api/models/sync_mira_data")
+      let data = await fetch.get(
+        "/api/models/" + stark.model_id + "/procedures"
+      );
+      commit("setProcedureData", data.data.procedures);
     }
   }
 });
