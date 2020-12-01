7844ef246ad7098793b8042347f319e1285b208d
zhuanglin450 <zhuanglin450@hotmail.com>
2020-12-02 02:43:50 +0800

    aes 加密。 encrypt

--- src/assets/js/tool.js
+++ src/assets/js/tool.js
@@ -88,4 +88,27 @@ function isStringEmpty(data) {
     return strRep.replace(/(^s*)|(s*$)/g, "").length == 0;
 }
 
-export { equal_deep, clone_deep, freeze_deep, isStringEmpty };
+/**
+ * 加密（需要先加载lib/aes/aes.min.js文件）
+ * @param word
+ * @returns {*}
+ */
+function encrypt(word){
+    var key = CryptoJS.enc.Utf8.parse("abcdefgabcdefg12");
+    var srcs = CryptoJS.enc.Utf8.parse(word);
+    var encrypted = CryptoJS.AES.encrypt(srcs, key, {mode:CryptoJS.mode.ECB,padding: CryptoJS.pad.Pkcs7});
+    return encrypted.toString();
+}
+
+/**
+ * 解密
+ * @param word
+ * @returns {*}
+ */
+function decrypt(word){
+    var key = CryptoJS.enc.Utf8.parse("abcdefgabcdefg12");
+    var decrypt = CryptoJS.AES.decrypt(word, key, {mode:CryptoJS.mode.ECB,padding: CryptoJS.pad.Pkcs7});
+    return CryptoJS.enc.Utf8.stringify(decrypt).toString();
+}
+
+export { equal_deep, clone_deep, freeze_deep, isStringEmpty,encrypt,decrypt};
